129c85997b976c9f11caa48cb7e931add576c854
JayanakaWijethunga <jayanaka987@gmail.com>
2019-09-13 22:42:17 -0700

    create resource controller

--- app/Http/Controllers/Api/UserDetailController.php
+++ app/Http/Controllers/Api/UserDetailController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\UserDetailResource;
+
+use App\User_detail;
 
 class UserDetailController extends Controller
 {
-    //
+    public function show(User_detail $user_detail):UserDetailResource
+
+    {
+
+        return new UserDetailResource($user_detail);
+        
+    }
 }
