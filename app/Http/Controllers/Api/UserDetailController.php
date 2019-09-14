ebe446e24e5ad54aa2c2a84fd1f6fc59fc3707ea
JayanakaWijethunga <jayanaka987@gmail.com>
2019-09-13 23:31:00 -0700

    create api authentication

--- app/Http/Controllers/Api/UserDetailController.php
+++ app/Http/Controllers/Api/UserDetailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\UserDetailResource;
@@ -10,11 +11,10 @@ use App\User_detail;
 
 class UserDetailController extends Controller
 {
-    public function show(User_detail $user_detail):UserDetailResource
-
+    public function index():UserDetailResource
     {
 
-        return new UserDetailResource($user_detail);
-        
+        return new UserDetailResource(User_detail::find(auth()->guard('api')->user()->id));
+
     }
 }
